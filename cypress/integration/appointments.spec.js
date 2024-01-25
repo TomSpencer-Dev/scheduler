84c075f68cbae388bf5e29b12e138a4c9b7c6407
Tom Spencer <thomas.spencer2034@gmail.com>
2024-01-25 08:54:06 -0500

    Add should navigate to Tuesday test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,14 @@
+describe("Appointments", () => {
+  it("Should book an interview", () => {
+    cy.visit("/");
+    cy.get;
+  });
+
+  it("Should edit an interview", () => {
+    cy.visit("/");
+  });
+
+  it("Should cancel an interview", () => {
+    cy.visit("/");
+  });
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,12 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  it("Should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.get("li").contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+  
 });
